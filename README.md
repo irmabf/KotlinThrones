b0d11d7b77e6b9a96adc728fe0a2789c6e36ebed
Irma <irblancofdz@gmail.com>
2018-05-17 04:29:02 +0200

    Two activities and model

---
+++ README.md
@@ -0,0 +1,3 @@
+# AppOfThrones
+
+Repositorio de la aplicación de Android del curso Kotlin desde 0 de Keepcoding
---
+++ app/src/main/java/com/example/irmablanco/appofthrones/CharactersRepo.kt
@@ -0,0 +1,40 @@
+package com.example.irmablanco.appofthrones
+
+object CharactersRepo {
+    val character: MutableList<Character> = mutableListOf()
+
+    get() {
+        if (field.isEmpty())
+            field.addAll(dummyCharacters())
+
+        return  field
+    }
+
+    private  fun dummyCharacters(): MutableList<Character> {
+
+        return (1..10).map {
+
+            intToCharacter(it)
+
+        }.toMutableList()
+
+    }
+    private  fun intToCharacter(int: Int): Character {
+        return Character(
+                name = "Personaje ${int}",
+                title = "Título ${int}",
+                born = "Nació en ${int}",
+                actor = "Actor ${int}",
+                quote = "Frase ${int}",
+                father = "Padre ${int}",
+                mother = "Madre ${int}",
+                spouse = "Esposa ${int}",
+                house = House(
+                        name = "Casa ${int}",
+                        region = "Region ${int}",
+                        words = "Lema ${int}"
+                )
+        )
+
+    }
+}
