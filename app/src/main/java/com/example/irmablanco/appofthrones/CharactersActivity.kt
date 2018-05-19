407e0b741fa5c7414600e5b9949ae6415db6e14c
Irma <irblancofdz@gmail.com>
2018-05-19 18:24:33 +0200

    Refactor CharactersActivity code

--- app/src/main/java/com/example/irmablanco/appofthrones/CharactersActivity.kt
+++ app/src/main/java/com/example/irmablanco/appofthrones/CharactersActivity.kt
@@ -10,15 +10,21 @@ import android.view.View
 import android.widget.Button
 
 class CharactersActivity: AppCompatActivity() {
+
+    val list: RecyclerView by lazy {
+        val list: RecyclerView = findViewById(R.id.list)
+        list.layoutManager = LinearLayoutManager(this)
+        list
+    }
+    val adapter: CharactersAdapter by lazy {
+        val adapter = CharactersAdapter { item, position ->
+            showDetails()
+        }
+        adapter
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_characters)
-        val list: RecyclerView = findViewById(R.id.list)
-        val adapter = CharactersAdapter() { item, position ->
-            showDetails()
-        }
-
-        list.layoutManager = LinearLayoutManager(this)
         list.adapter = adapter
         val characters: MutableList<Character> = CharactersRepo.character
         adapter.setCharacters(characters)
